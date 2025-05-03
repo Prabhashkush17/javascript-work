45867d633f5dec0155d4ef968d170e2379c32fc8
prabhashkush17 <prabhashkush17@gmail.com>
2025-05-03 13:53:25 +0530

    my commit

---
+++ 03-05-2025/script.js
@@ -0,0 +1,8 @@
+const mystring = "developer";
+
+// console.log(mystring.charAt(0))
+let mynewstring = mystring.charAt(0).toUpperCase()+ mystring.substring(1);
+console.log(mynewstring);
+let x= 10;
+
+
