704ef13a397a6639c92af77dbb985f6f95736b43
prabhashkush17 <prabhashkush17@gmail.com>
2025-05-05 13:33:06 +0530

    js math methods

---
+++ 05-05-2025/script.js
@@ -0,0 +1,33 @@
+/*let x= Math.PI;
+
+x= Math.sqrt(49);
+
+x= Math.trunc(25648.4567);
+
+x= Math.min(2,-85,56,89,-5);
+
+x= Math.max(45,708,-58,45,2,0);
+
+
+x= Math.pow(2,3);
+
+x= Math.floor(Math.random()*100)+1;
+
+
+console.log(x);*/
+
+let x= Math.trunc(Math.random()*100)+1;
+
+let y=  Math.trunc(Math.random()*50)+1;
+
+let sum=x+y;
+let difference=x-y;
+let product=x*y;
+let quotient=x/y;
+let remainder=x%y;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
