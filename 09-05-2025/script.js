77bbada780b2dd10c26a67ff4b15979dd8ecafc5
prabhashkush17 <prabhashkush17@gmail.com>
2025-05-09 13:32:22 +0530

    prompt use

---
+++ 09-05-2025/script.js
@@ -0,0 +1,4 @@
+let length= prompt(" Enter a lenght of rectangle : ");
+let breadth= prompt( " Enter a breadth of rectangle");
+let area= length*breadth;
+console.log( "The Area of a rectangle is : " + area);
