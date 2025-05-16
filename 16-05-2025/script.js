ca578f4540afe5a2bd3bd2ef997f2cfc3f725425
prabhashkush17 <prabhashkush17@gmail.com>
2025-05-16 13:30:04 +0530

    function

---
+++ 16-05-2025/script.js
@@ -0,0 +1,42 @@
+// fuctions are the block of code that uses for reusability of set of codes....
+
+//function identifire (variable name)(value catch / parameter){
+
+    // code
+//}
+// 1...function decleration
+// 2...function defination
+// 3...function calling
+
+    //identifire(value pass /arrgument)
+
+    // function Demo(){
+    //     console.log("hello function")
+
+    // }
+    // Demo();
+
+// wap to take a input from a user and check the number is odd or even using function...
+
+// function Check (){
+//     let num = prompt(" Enter a number : ");
+//      if (num%2!==0){
+//         console.log(" Number is odd ")
+//      }
+//      else{
+//         console.log( " Number is Even ")
+//      }
+// }
+
+// Check();
+
+// WAP to take a input from the user and print reverse table of that number.....
+
+function Reverse (){
+let num1= prompt(" Enter a number : " )
+for (let i=10; i>=0; i--){
+    
+    console.log(num1*i);
+}
+}
+Reverse();
