ee0fd06251af5b96c1e4b725f724db8527b35b66
prabhashkush17 <prabhashkush17@gmail.com>
2025-05-17 13:33:13 +0530

    firstcommit

---
+++ 17-05-2025 functions/scriprt.js
@@ -0,0 +1,76 @@
+// // // there are four types of function....
+
+// // //(1) no return no parameter.....
+// // function display1(){
+// //     console.log(10+20);
+
+// // }  
+// // display1();
+
+// // //(2) return with no parameter...
+// // function display2 (){
+// //     return "hello";
+// // }
+// // let result = display2();
+// // console.log(result);
+// // alert(result);
+
+// // //(3) parameter with no return....
+// // function display3(a,b){
+// //     console.log(a + b);
+// // }
+// // display3(10,30);
+
+// //(4) parameters with return.....
+// // function dispaly4(x,y){
+// //     return x*y;
+// // }
+// // let result2 = dispaly4(5,20)
+// // console.log(result2);
+
+// // task 
+// // let name = prompt(" Enter your name:")
+// // function show(){
+// //     console.log(name);
+// // }
+// // show();
+
+// // function color (){
+// //     return " red";
+// // } 
+// // console.log(color());
+
+
+// function cube(n){
+//  n=n*n*n;
+
+// }
+// console.log(cube(5));
+
+// function user1(person){
+//     console.log(`person name is ${person.name}and age is ${person.age}`)
+// }
+
+// user1({
+// name: " prabhash";
+// age: 34
+// });
+
+// function expression....
+// let myfunction = function() {
+// console.log(" this is a function expression")
+// };
+// myfunction();
+
+// task
+//  let substraction = function(a,b){
+//     console.log(a-b);
+//  }
+//  substraction(10,5);
+
+// arrow function.....
+
+let myfunction2 = ()=>{
+    console.log(" this is an arrow function")
+}
+myfunction2();
