af3a366b42c72f636acdd9d3bacdf5abbe07454a
Prabhash kumar <prabhashkush17@gmail.com>
2025-05-19 23:21:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// // write a regular fuction that takes a strings and returs it with the first letter capitalize...
+// function capitalizeFirstLetter (str){
+   
+//    console.log(str.charAt().toUpperCase() + str.slice(1));
+// }
+// capitalizeFirstLetter("prabhash");
+
+// // Show an alert message that says " please login" after 5 second on your website
+
+// setTimeout(function(){
+//     console.log(alert(" Please Login "))
+// },5000);
+
+// Make an arrow function that takes a price and a discount and return the price after discount....
+
+// let discountedPrice = (a,b)=>{
+// let c = a-a*(b/100);
+// return c;
+// }
+// let result = discountedPrice(100,20);
+// console.log(result);
+
+// Create a function that builds a username from a full name.
