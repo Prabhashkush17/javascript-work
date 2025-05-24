ab529fe4910be12600d79bfdf44d82e68cd25572
Prabhash kumar <prabhashkush17@gmail.com>
2025-05-24 13:31:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-// // write a regular fuction that takes a strings and returs it with the first letter capitalize...
-// function capitalizeFirstLetter (str){
-   
-//    console.log(str.charAt().toUpperCase() + str.slice(1));
+let h1 = document.getElementById("demo");
+h1.style.color = "red"
+h1.style.backgroundColor = "yellow";
+
+// color, background color, border, border radius, font size,
+// font style - italic
+
+let para = document.getElementById("para")
+// para.style.color = "green";
+// para.style.background-Color = "yellow";
+// para.style.border = "yellow";
+// para.style.borderRadius = "yellow";
+
+para.style.cssText = "color : pink; background-color: blue;border:10px solid red; border-radius: 40px, font-size:40px, font-style: italic;";
+
+// function change(){
+// let input = document.getElementById("myinput")
+// input.value = "delhi";
 // }
-// capitalizeFirstLetter("prabhash");
+function printtext(){
+    let input = document.getElementById("myinput").value
+    let h1 = document.getElementById("newtext");
+    h1.innerHTML = input;
+}
 
-// // Show an alert message that says " please login" after 5 second on your website
 
-// setTimeout(function(){
-//     console.log(alert(" Please Login "))
-// },5000);
+// the addEventListener() method attaches an event handler to the specified element.
 
-// Make an arrow function that takes a price and a discount and return the price after discount....
-
-// let discountedPrice = (a,b)=>{
-// let c = a-a*(b/100);
-// return c;
-// }
-// let result = discountedPrice(100,20);
-// console.log(result);
-
-// Create a function that builds a username from a full name.
+let button = document.getElementById("btn");
+button.addEventListener("click", function(){
+    console.log("button clicked");
+    h1.style.color = "blue";
+})
