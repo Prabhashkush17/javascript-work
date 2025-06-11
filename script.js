e353e6c014e8711afa8fe1ec18d3b453933463e9
Prabhash kumar <prabhashkush17@gmail.com>
2025-06-11 22:01:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+/*let x= Math.PI;
+
+x= Math.sqrt(49);
+
+x= Math.trunc(25648.4567);
+
+x= Math.min(2,-85,56,89,-5);
+
+x= Math.max(45,708,-58,45,2,0);
 
 
-// console.log("starting");
+x= Math.pow(2,3);
 
-// setTimeout(()=>{
-
-// console.log("pending");
-// })
+x= Math.floor(Math.random()*100)+1;
 
 
+console.log(x);*/
 
-// console.log("Ending");
+let x= Math.trunc(Math.random()*100)+1;
 
-let btn1;
-let sum = 0;
-let btn = ()=>{
-     btn1 = setInterval(()=>{
-        console.log(sum++)
-    },1000)
-}
-let btn2 = ()=>{
-    clearInterval(btn1)
-}
+let y=  Math.trunc(Math.random()*50)+1;
+
+let sum=x+y;
+let difference=x-y;
+let product=x*y;
+let quotient=x/y;
+let remainder=x%y;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
